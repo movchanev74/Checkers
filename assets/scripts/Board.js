629d8cfd1b503aad545ab0ae316ed44cf7220e65
movchanev74 <movchanev74@gmail.com>
2019-03-22 21:40:06 +0500

    add Scene, Prefabs(checkers, cell) and update Board
    create functioon CreateCheckers in board.js

--- assets/scripts/Board.js
+++ assets/scripts/Board.js
@@ -12,134 +12,129 @@ cc.Class({
     extends: cc.Component,
 
     properties: {
+    	checkersArray: {
+            default: [],
+            type: cc.Node
+		},
     	spriteUp: {
-        default: null,
-        type: cc.SpriteFrame
+        	default: null,
+        	type: cc.SpriteFrame
     	},
     	spriteLeftUp: {
-        default: null,
-        type: cc.SpriteFrame
+        	default: null,
+       	 	type: cc.SpriteFrame
     	},
     	spriteLeft: {
-        default: null,
-        type: cc.SpriteFrame
+       	 	default: null,
+       		type: cc.SpriteFrame
     	},
     	spriteLeftDown: {
-        default: null,
-        type: cc.SpriteFrame
+       	 	default: null,
+        	type: cc.SpriteFrame
     	},
     	spriteDown: {
-        default: null,
-        type: cc.SpriteFrame
+       		default: null,
+       	 		type: cc.SpriteFrame
     	},
     	spriteRightDown: {
-        default: null,
-        type: cc.SpriteFrame
+        	default: null,
+        	type: cc.SpriteFrame
     	},
      	spriteRight: {
-        default: null,
-        type: cc.SpriteFrame
+        	default: null,
+        	type: cc.SpriteFrame
     	},
     	spriteRightUp: {
-        default: null,
-        type: cc.SpriteFrame
+        	default: null,
+        	type: cc.SpriteFrame
     	},
     	spriteWhiteWood: {
-        default: null,
-        type: cc.SpriteFrame
+        	default: null,
+        	type: cc.SpriteFrame
     	},
     	spriteBlackWood: {
-        default: null,
-        type: cc.SpriteFrame
-    	}
-        // foo: {
-        //     // ATTRIBUTES:
-        //     default: null,        // The default value will be used only when the component attaching
-        //                           // to a node for the first time
-        //     type: cc.SpriteFrame, // optional, default is typeof default
-        //     serializable: true,   // optional, default is true
-        // },
-        // bar: {
-        //     get () {
-        //         return this._bar;
-        //     },
-        //     set (value) {
-        //         this._bar = value;
-        //     }
-        // },
+        	default: null,
+        	type: cc.SpriteFrame
+    	},
+    	whiteChecker:{
+			default:null,
+			type:cc.Prefab
+		},
+    	blackChecker:{
+			default:null,
+			type:cc.Prefab
+		},
+		cell:{
+			default:null,
+			type:cc.Prefab
+		},
+		countCell:{
+			default:8,
+			type:cc.Integer
+		}
     },
 
-    // LIFE-CYCLE CALLBACKS:
+    onLoad () {
+    	this.checkersArray = new Array(this.countCell);
+		for (var i = 0; i < this.checkersArray.length; i++) {
+  			this.checkersArray [i] = new Array(this.countCell);
+		};
+    },
 
-    // onLoad () {},
+    click(pos){
+    	cc.log(pos);
+    },
+
     createBoard(countCell)
     {
+    	function createCell(sprite,pos,newCell){
+    		if(newCell == null)
+	    		newCell = new cc.Node();
+			let spriteComponent = newCell.addComponent(cc.Sprite);
+			spriteComponent.spriteFrame = sprite;
+			this.node.addChild(newCell);
+			newCell.position = pos;
+    	}
+
     	for (let i = -countCell/2; i < countCell/2; i++) {
-    		console.log(i);
-    		let down = new cc.Node();
-			let spriteComponent = down.addComponent(cc.Sprite);
-			spriteComponent.spriteFrame = this.spriteDown;
-			this.node.addChild(down);
-			down.position = new cc.Vec2(i*32, (-countCell/2-1)*32);
-
-			let up = new cc.Node();
-			spriteComponent = up.addComponent(cc.Sprite);
-			spriteComponent.spriteFrame = this.spriteUp;
-			this.node.addChild(up);
-			up.position = new cc.Vec2(i*32, countCell/2*32);
-
-			let left = new cc.Node();
-			spriteComponent = left.addComponent(cc.Sprite);
-			spriteComponent.spriteFrame = this.spriteLeft;
-			this.node.addChild(left);
-			left.position = new cc.Vec2((-countCell/2-1)*32, i*32);
-
-			let right = new cc.Node();
-			spriteComponent = right.addComponent(cc.Sprite);
-			spriteComponent.spriteFrame = this.spriteRight;
-			this.node.addChild(right);
-			right.position = new cc.Vec2(countCell/2*32, i*32);
+    		createCell.call(this, this.spriteDown,new cc.Vec2(i*32, (-countCell/2-1)*32));
+    		createCell.call(this, this.spriteUp,new cc.Vec2(i*32, countCell/2*32));
+    		createCell.call(this, this.spriteLeft,new cc.Vec2((-countCell/2-1)*32, i*32));
+			createCell.call(this, this.spriteRight,new cc.Vec2(countCell/2*32, i*32));
 		}
-
-		let rightUp = new cc.Node();
-		let spriteComponent = rightUp.addComponent(cc.Sprite);
-		spriteComponent.spriteFrame = this.spriteRightUp;
-		this.node.addChild(rightUp);
-		rightUp.position = new cc.Vec2(countCell/2*32, countCell/2*32);
-
-		let rightDown = new cc.Node();
-		spriteComponent = rightDown.addComponent(cc.Sprite);
-		spriteComponent.spriteFrame = this.spriteRightDown;
-		this.node.addChild(rightDown);
-		rightDown.position = new cc.Vec2(countCell/2*32, (-countCell/2-1)*32);
-
-		let leftUp = new cc.Node();
-		spriteComponent = leftUp.addComponent(cc.Sprite);
-		spriteComponent.spriteFrame = this.spriteLeftUp;
-		this.node.addChild(leftUp);
-		leftUp.position = new cc.Vec2((-countCell/2-1)*32, countCell/2*32);
-
-		let leftDown = new cc.Node();
-		spriteComponent = leftDown.addComponent(cc.Sprite);
-		spriteComponent.spriteFrame = this.spriteLeftDown;
-		this.node.addChild(leftDown);
-		leftDown.position = new cc.Vec2((-countCell/2-1)*32, (-countCell/2-1)*32); 
+		createCell.call(this, this.spriteRightUp,new cc.Vec2(countCell/2*32, countCell/2*32));
+    	createCell.call(this, this.spriteRightDown,new cc.Vec2(countCell/2*32, (-countCell/2-1)*32));
+    	createCell.call(this, this.spriteLeftUp,new cc.Vec2((-countCell/2-1)*32, countCell/2*32));
+		createCell.call(this, this.spriteLeftDown,new cc.Vec2((-countCell/2-1)*32, (-countCell/2-1)*32));
 
 		for (let i = -countCell/2; i < countCell/2; i++) 
-			for (let j = -countCell/2; j < countCell/2; j++) {
-    		let cell = new cc.Node();
-			let spriteComponent = cell.addComponent(cc.Sprite);
-			if((1+i+j)%2 == 0)
-				spriteComponent.spriteFrame = this.spriteWhiteWood;
-			else
-				spriteComponent.spriteFrame = this.spriteBlackWood;
-			this.node.addChild(cell);
-			cell.position = new cc.Vec2(i*32, j*32);
-		}
+			for (let j = -countCell/2; j < countCell/2; j++)
+				if((1+i+j)%2 == 0)
+					createCell.call(this, this.spriteWhiteWood,new cc.Vec2(i*32, j*32),cc.instantiate(this.cell));
+				else
+					createCell.call(this, this.spriteBlackWood,new cc.Vec2(i*32, j*32),cc.instantiate(this.cell));
     },
-    
+    addCheckers(){
+    	function createCell(checker,pos,parent){
+    		this.checkersArray[pos.x][pos.y] = cc.instantiate(checker);
+    		this.checkersArray[pos.x][pos.y].position = new cc.Vec2((pos.x - this.countCell/2)*32, (pos.y - this.countCell/2)*32);
+    		this.node.addChild(this.checkersArray[pos.x][pos.y]);
+    	}
+
+    	let parent = new cc.Node();
+    	parent.name = "Checkers";
+    	for (let x = 0; x < this.countCell; x++) 
+			for (let y = 0; y < this.countCell; y++){
+    			if(y >= 0 && y < 3 && ((1+x+y)%2 == 0))
+    				createCell.call(this, this.blackChecker, new cc.Vec2(x, y), parent);
+    			if(y >= 5 && y < 8 && ((1+x+y)%2 == 0))
+    				createCell.call(this, this.whiteChecker, new cc.Vec2(x, y), parent);
+    		}
+    },
+
     start () {
-    	this.createBoard(14);
+    	this.createBoard(this.countCell);
+    	this.addCheckers();
     },
 
     // update (dt) {},
